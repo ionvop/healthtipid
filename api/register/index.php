b7472503089e39261c8bb90e11ef60dd32831ed6
ionvop <ionvop@gmail.com>
2025-09-30 17:48:18 +0800

    Update index.php

--- api/register/index.php
+++ api/register/index.php
@@ -6,53 +6,64 @@ header("Content-Type: application/json");
 $db = new SQLite3("database.db");
 
 try {
-    $_POST = json_decode(file_get_contents('php://input'), true);
-    session_start();
+    switch ($_SERVER["REQUEST_METHOD"]) {
+        case "POST":
+            $_POST = json_decode(file_get_contents('php://input'), true);
+            session_start();
 
-    if ($_POST["firstname"] == "" || $_POST["lastname"] == "" || $_POST["birthdate"] == "" || $_POST["gender"] == "") {
-        http_response_code(400);
+            if ($_POST["firstname"] == "" || $_POST["lastname"] == "" || $_POST["birthdate"] == "" || $_POST["gender"] == "") {
+                http_response_code(400);
 
-        echo json_encode([
-            "error" => "Missing fields"
-        ]);
+                echo json_encode([
+                    "error" => "Missing fields"
+                ]);
 
-        exit;
+                exit;
+            }
+
+            $query = <<<SQL
+                INSERT INTO `users` (`email`, `firstname`, `lastname`, `middleinitial`, `birthdate`, `gender`, `email`, `phone`, `province`, `city`, `barangay`, `street`)
+                VALUES (:email, :firstname, :lastname, :middleinitial, :birthdate, :gender, :email, :phone, :province, :city, :barangay, :street)
+            SQL;
+
+            $stmt = $db->prepare($query);
+            $stmt->bindValue(":email", $_SESSION["email"]);
+            $stmt->bindValue(":firstname", $_POST["firstname"]);
+            $stmt->bindValue(":lastname", $_POST["lastname"]);
+            $stmt->bindValue(":middleinitial", $_POST["middleinitial"]);
+            $stmt->bindValue(":birthdate", $_POST["birthdate"]);
+            $stmt->bindValue(":gender", $_POST["gender"]);
+            $stmt->bindValue(":phone", $_POST["phone"]);
+            $stmt->bindValue(":province", $_POST["province"]);
+            $stmt->bindValue(":city", $_POST["city"]);
+            $stmt->bindValue(":barangay", $_POST["barangay"]);
+            $stmt->bindValue(":street", $_POST["street"]);
+            $stmt->execute();
+            $session = uniqid("session-");
+
+            $query = <<<SQL
+                UPDATE `users` SET `session` = :session WHERE `email` = :email
+            SQL;
+
+            $stmt = $db->prepare($query);
+            $stmt->bindValue(":session", $session);
+            $stmt->bindValue(":email", $_SESSION["email"]);
+            $stmt->execute();
+
+            echo json_encode([
+                "session" => $session
+            ]);
+
+            exit;
+        default:
+            http_response_code(405);
+
+            echo json_encode([
+                "error" => "Method not allowed"
+            ]);
+
+            exit;
     }
-
-    $query = <<<SQL
-        INSERT INTO `users` (`email`, `firstname`, `lastname`, `middleinitial`, `birthdate`, `gender`, `email`, `phone`, `province`, `city`, `barangay`, `street`)
-        VALUES (:email, :firstname, :lastname, :middleinitial, :birthdate, :gender, :email, :phone, :province, :city, :barangay, :street)
-    SQL;
-
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(":email", $_SESSION["email"]);
-    $stmt->bindValue(":firstname", $_POST["firstname"]);
-    $stmt->bindValue(":lastname", $_POST["lastname"]);
-    $stmt->bindValue(":middleinitial", $_POST["middleinitial"]);
-    $stmt->bindValue(":birthdate", $_POST["birthdate"]);
-    $stmt->bindValue(":gender", $_POST["gender"]);
-    $stmt->bindValue(":phone", $_POST["phone"]);
-    $stmt->bindValue(":province", $_POST["province"]);
-    $stmt->bindValue(":city", $_POST["city"]);
-    $stmt->bindValue(":barangay", $_POST["barangay"]);
-    $stmt->bindValue(":street", $_POST["street"]);
-    $stmt->execute();
-    $session = uniqid("session-");
-
-    $query = <<<SQL
-        UPDATE `users` SET `session` = :session WHERE `email` = :email
-    SQL;
-
-    $stmt = $db->prepare($query);
-    $stmt->bindValue(":session", $session);
-    $stmt->bindValue(":email", $_SESSION["email"]);
-    $stmt->execute();
-
-    echo json_encode([
-        "session" => $session
-    ]);
-
-    exit;
 } catch (Exception $e) {
     http_response_code(500);
 
