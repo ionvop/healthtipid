5a6eac6765d3e62e5f5a50be83a178daeddca8a4
ionvop <ionvop@gmail.com>
2025-09-30 16:00:58 +0800

    Update index.php

--- api/user/index.php
+++ api/user/index.php
@@ -3,9 +3,6 @@
 chdir("../");
 require_once "common.php";
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
 $db = new SQLite3("database.db");
 
 try {
@@ -34,6 +31,66 @@ try {
 
             echo json_encode([
                 "user" => $user
+            ]);
+
+            exit;
+        case "POST":
+            $_POST = json_decode(file_get_contents('php://input'), true);
+            $headers = getallheaders();
+            $session = $headers["Authorization"];
+            $user = getUser($session);
+
+            if ($user == false) {
+                http_response_code(403);
+
+                echo json_encode([
+                    "error" => "User not found"
+                ]);
+
+                exit;
+            }
+
+            if ($_POST["firstname"] == "" || $_POST["lastname"] == "" || $_POST["birthdate"] == "" || $_POST["gender"] == "") {
+                http_response_code(400);
+
+                echo json_encode([
+                    "error" => "Missing fields"
+                ]);
+
+                exit;
+            }
+
+            $query = <<<SQL
+                UPDATE `users`
+                SET `firstname` = :firstname,
+                `lastname` = :lastname,
+                `middleinitial` = :middleinitial,
+                `birthdate` = :birthdate,
+                `gender` = :gender,
+                `phone` = :phone,
+                `province` = :province,
+                `city` = :city,
+                `barangay` = :barangay,
+                `street` = :street
+                WHERE `id` = :id
+            SQL;
+
+            $stmt = $db->prepare($query);
+            $stmt->bindValue(":id", $user["id"]);
+            $stmt->bindValue(":firstname", $_POST["firstname"]);
+            $stmt->bindValue(":lastname", $_POST["lastname"]);
+            $stmt->bindValue(":middleinitial", $_POST["middleinitial"]);
+            $stmt->bindValue(":birthdate", $_POST["birthdate"]);
+            $stmt->bindValue(":gender", $_POST["gender"]);
+            $stmt->bindValue(":phone", $_POST["phone"]);
+            $stmt->bindValue(":province", $_POST["province"]);
+            $stmt->bindValue(":city", $_POST["city"]);
+            $stmt->bindValue(":barangay", $_POST["barangay"]);
+            $stmt->bindValue(":street", $_POST["street"]);
+            $stmt->execute();
+
+            echo json_encode([
+                "success" => true
             ]);
 
             exit;
